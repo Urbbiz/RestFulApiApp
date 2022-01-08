398e3bc12804d705b9c9f946bc3ba5159ca6607c
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2022-01-08 14:14:50 +0200

    DataCotext class added

---
+++ Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestFulApiApp.Models;
+
+namespace RestFulApiApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+         
+        public DbSet<Dish>?  Dishes { get; set; }
+        public DbSet<Fruit>? Fruits { get; set; }
+        public DbSet<Vegetable> Vegetables { get; set; }
+    }
+}
